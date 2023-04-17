1c4ec1d56edc0eb508196341e9342f0b3e8529f7
devMarcus21 <marcusaleclee2000@gmail.com>
2023-04-17 13:45:15 -0700

    Editing init.lua for what worked on ubuntu vm

--- init.lua
+++ init.lua
@@ -1,13 +1,19 @@
 -- windows path = ~\AppData\Local\nvim\init.lua
 -- linux path = ~\.config\nvim\init.lua
-vim.cmd.packadd('packer.nvim')
 
-vim.cmd "colorscheme nord"
+vim.cmd [[packadd packer.nvim]]
+
+vim.cmd [[colorscheme nord]]
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 vim.opt.guicursor = ""
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
-  use 'shaunsingh/nord.nvim'
+
+   use({
+    "shaunsingh/nord.nvim", -- Colorscheme
+    commit = "78f5f001709b5b321a35dcdc44549ef93185e024",
+  })
+  --use 'shaunsingh/nord.nvim'
 end)
