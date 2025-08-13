aa553ccddd1ccd4e5a7afc3cdb696325aaa0dd60
devMarcus21 <marcusaleclee2000@gmail.com>
2025-08-12 17:47:28 -0700

    Updating init.lua to add treesitter

--- init.lua
+++ init.lua
@@ -1,6 +1,3 @@
--- windows path = ~\AppData\Local\nvim\init.lua
--- linux path = ~\.config\nvim\init.lua
-
 vim.cmd [[packadd packer.nvim]]
 
 vim.cmd [[colorscheme nord]]
@@ -8,12 +5,41 @@ vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 vim.opt.guicursor = ""
 
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "rust", "go", "javascript" },
+  sync_install = false,
+  auto_install = false,
+  ignore_install = { },
+
+  highlight = {
+    enable = true,
+    disable = { },
+    --Special disable for large file sizes
+    --disable = function(lang, buf)
+        --local max_filesize = 100 * 1024 -- 100 KB
+        --local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        --if ok and stats and stats.size > max_filesize then
+            --return true
+        --end
+    --end,
+    additional_vim_regex_highlighting = false,
+  },
+}
+
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
    use({
     "shaunsingh/nord.nvim", -- Colorscheme
     commit = "78f5f001709b5b321a35dcdc44549ef93185e024",
+  })
+
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+    end,
   })
   --use 'shaunsingh/nord.nvim'
 end)
